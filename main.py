20ac6258e32c101cde8f0df024d704fe8b2ca955
JuanVargas <juansvc2004@gmail.com>
2024-09-03 23:23:02 -0300

    Adicionando opção de Depositar

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+import time
 import keyboard
+
+movimentacoes = []
 
 def limpar():
     os.system("cls" if os.name == "nt" else "clear")
 
 def printcenter(x):
-    print(x.center(75))
+    print(x.center(83))
 
 def main():
 
@@ -26,12 +29,13 @@ def main():
         '''
 
 
-        print(paybank) #75
+        print(paybank) #83
 
 
         printcenter("Bem Vindo ao PayBank!! O que deseja fazer por agora?\n")
 
-        opcoes = '''
+        opcoes = '''\n
+        Escolha uma das opções abaixo...
         — — — — — — — — — — — — — — —
         |   [1] - Depositar         |
         |   [2] - Sacar             |
@@ -46,8 +50,31 @@ def main():
 
         tecla = keyboard.read_event(suppress=True).name
 
-        if tecla:
+        if tecla == "1":
+            printcenter("Opção \"Depositar\" foi escolhida\n\n")
+            deposito = float(input("Quanto deseja depositar? R$"))
+            if deposito > 0:
+                movimentacoes.append(f"Deposito: R$ {deposito:.2f}")
+            else:
+                printcenter("Valor inválido. Tente novamente.")
+                time.sleep(.8)
+                continue    
             break
 
+        elif tecla == "2":
+            printcenter("Opção \"Sacar\" foi escolhida\n\n")
+            break
+
+        elif tecla == "3":
+            printcenter("Opção \"Ver Extrato\" foi escolhida\n\n")
+            break
+
+        elif tecla == "0":
+            printcenter("Saindo do PayBank...")
+            break
+
+        else:
+            printcenter("Opção inválida. Tente novamente.")
+            time.sleep(.8)
 
 main()
